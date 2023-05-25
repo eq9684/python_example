19c6896a4aad8cea1e0bac3419110bcc837176d5
Hua Wang <eq9684@hotmail.com>
2023-05-25 14:10:51 +0800

    Add files via upload

---
+++ custom_metric.py
@@ -0,0 +1,23 @@
+import subprocess
+import boto3
+
+output = subprocess.check_output('df -h | grep /dev/nvme | awk \'{print  $5}\'', shell=True)
+metric = float(output.decode('utf-8').strip().strip('%'))/100
+print (metric)
+
+client = boto3.client('cloudwatch')
+response = client.put_metric_data(
+    Namespace = 'my_custom_metric',
+    MetricData = [
+        {
+            'MetricName': 'DiskUsage',
+            'Dimensions':[
+                {
+                    'Name': 'DiskUsage',
+                    'Value': '/'
+                }
+            ],
+            'Value': metric
+        }
+    ]
+)
