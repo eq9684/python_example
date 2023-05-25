1c1861f814f44e65ece5e557c382786888d00150
Hua Wang <eq9684@hotmail.com>
2023-05-25 14:42:58 +0800

    Update custom_cloudwatch_metric.py

--- custom_cloudwatch_metric.py
+++ custom_cloudwatch_metric.py
@@ -2,10 +2,12 @@ import subprocess
 import boto3
 
 hostname = subprocess.check_output('hostname', shell=True).decode('utf-8').strip()
-output = subprocess.check_output('df -h | grep /dev/nvme | awk \'{print  $5}\'', shell=True)
+output = subprocess.check_output('df -h | grep /dev/nvme | awk \'{print $5}\'', shell=True)
 metric = float(output.decode('utf-8').strip().strip('%'))/100
+memfree = round(int(subprocess.check_output('cat /proc/meminfo | grep MemFree | awk \'{print $2}\'', shell=True).decode('utf-8').strip())/1024, 1)
 print (hostname)
 print (metric)
+print (memfree, 'MB Memory Free')
 
 client = boto3.client('cloudwatch')
 response = client.put_metric_data(
