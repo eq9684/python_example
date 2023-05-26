8c03b34c89be7dddf4e476e89ce247a6b0b54f10
Hua Wang <eq9684@hotmail.com>
2023-05-26 08:08:01 +0800

    Update check_proc.py

--- check_proc.py
+++ check_proc.py
@@ -3,7 +3,7 @@ import sys
 import boto3
 
 #usage: python3 check_proc.py process_name
-output = subprocess.check_output('ps -ef | grep ' + str(sys.argv[1]) + ' | grep -v grep | grep -v report_proc | awk \'{print $2}\'', shell=True)
+output = subprocess.check_output('ps -ef | grep ' + str(sys.argv[1]) + ' | grep -v grep | grep -v check_proc | awk \'{print $2}\'', shell=True)
 metric = output.decode('utf-8').strip().strip('%').split()
 if len(metric) == 0:
     print ('process not exist')
